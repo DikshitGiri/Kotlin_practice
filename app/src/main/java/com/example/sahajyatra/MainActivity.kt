65d871b99058190a98b7f70c659ba35cc9df4192
Dikshit <dikshit@MacBook-Air.local>
2026-03-31 22:34:32 +0900

    drawyer

--- app/src/main/java/com/example/sahajyatra/MainActivity.kt
+++ app/src/main/java/com/example/sahajyatra/MainActivity.kt
@@ -4,44 +4,57 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.outlined.DateRange
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.ShoppingCart
+import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color as ComposeColor
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.toRoute
 import com.example.sahajyatra.ui.theme.SahajYatraTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
@@ -57,109 +70,153 @@ class MainActivity : ComponentActivity() {
 }
 
 @Serializable
-data object Home
-
-@Serializable
-data class ColorRoute(val name: String, val value: Long)
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun ColorScreen(navController: NavController, colorName: String, colorValue: Long) {
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = { Text(text = "Color Screen") },
-                navigationIcon = {
-                    IconButton(onClick = { navController.navigateUp() }) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = "Back"
-                        )
-                    }
-                }
-            )
-        }
-    ) { padding ->
-        Box(
-            modifier = Modifier
-                .padding(padding)
-                .fillMaxSize()
-                .background(ComposeColor(colorValue)),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = "$colorName SCREEN", fontSize = 24.sp, color = ComposeColor.White)
-        }
-    }
+sealed class Destination(val label: String) {
+    @Serializable
+    data object Home : Destination("Home")
+    @Serializable
+    data object Shopping : Destination("Cart")
+    @Serializable
+    data object Favorites : Destination("Favorites")
+    @Serializable
+    data object Calendar : Destination("Calendar")
+    @Serializable
+    data object Bin : Destination("Bin")
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun HomeScreen(navController: NavController) {
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = { Text(stringResource(R.string.app_name)) },
-                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.surfaceContainer
-                )
-            )
-        }
-    ) { padding ->
-        Column(
-            modifier = Modifier
-                .padding(padding)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            ColorBox(
-                name = "RED",
-                color = ComposeColor(0xFFFF0000),
-                onClick = { navController.navigate(ColorRoute("RED", 0xFFFF0000)) }
-            )
-            Spacer(modifier = Modifier.height(24.dp))
-            ColorBox(
-                name = "GREEN",
-                color = ComposeColor(0xFF00FF00),
-                onClick = { navController.navigate(ColorRoute("GREEN", 0xFF00FF00)) }
-            )
-            Spacer(modifier = Modifier.height(24.dp))
-            ColorBox(
-                name = "BLUE",
-                color = ComposeColor(0xFF0000FF),
-                onClick = { navController.navigate(ColorRoute("BLUE", 0xFF0000FF)) }
-            )
-        }
-    }
+sealed class NavigationDrawer(
+    val label: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val route: Destination
+) {
+    data object Home : NavigationDrawer(
+        "Home", Icons.Filled.Home, Icons.Outlined.Home, Destination.Home
+    )
+    data object Shopping : NavigationDrawer(
+        "Cart", Icons.Filled.ShoppingCart, Icons.Outlined.ShoppingCart, Destination.Shopping
+    )
+    data object Favorites : NavigationDrawer(
+        "Favorites", Icons.Filled.Favorite, Icons.Outlined.FavoriteBorder, Destination.Favorites
+    )
+    data object Calendar : NavigationDrawer(
+        "Calendar", Icons.Filled.DateRange, Icons.Outlined.DateRange, Destination.Calendar
+    )
+    data object Bin : NavigationDrawer(
+        "Bin", Icons.Filled.Delete, Icons.Outlined.Delete, Destination.Bin
+    )
 }
 
 @Composable
-fun ColorBox(name: String, color: ComposeColor, onClick: () -> Unit) {
+fun ContentScreen(name: String) {
     Box(
-        modifier = Modifier
-            .size(150.dp)
-            .border(4.dp, color, shape = RoundedCornerShape(16.dp))
-            .clickable(onClick = onClick)
-            .padding(16.dp),
+        modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = name, color = color, fontSize = 24.sp)
+        Text(name, fontSize = 28.sp)
     }
 }
 
 @Composable
 fun NavigationApp() {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val coroutineScope = rememberCoroutineScope()
     val navController = rememberNavController()
-    NavHost(
-        navController = navController,
-        startDestination = Home
-    ) {
-        composable<Home> {
-            HomeScreen(navController)
+    
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    val items = listOf(
+        NavigationDrawer.Home,
+        NavigationDrawer.Shopping,
+        NavigationDrawer.Favorites,
+        NavigationDrawer.Calendar,
+        NavigationDrawer.Bin
+    )
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                items.forEach { item ->
+                    val isSelected = currentDestination?.hierarchy?.any { it.hasRoute(item.route::class) } == true
+                    NavigationDrawerItem(
+                        label = { Text(item.label) },
+                        selected = isSelected,
+                        onClick = {
+                            navController.navigate(item.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                            coroutineScope.launch {
+                                drawerState.close()
+                            }
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = if (isSelected) item.selectedIcon else item.unselectedIcon,
+                                contentDescription = item.label
+                            )
+                        },
+                        modifier = Modifier.padding(horizontal = 12.dp, vertical = 4.dp)
+                    )
+                }
+            }
         }
-        composable<ColorRoute> { navBackStackEntry ->
-            val color = navBackStackEntry.toRoute<ColorRoute>()
-            ColorScreen(navController, color.name, color.value)
+    ) {
+        NavigationDrawerHost(coroutineScope, drawerState, navController)
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun NavigationDrawerHost(
+    coroutineScope: CoroutineScope,
+    drawerState: DrawerState,
+    navController: NavHostController
+) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(stringResource(R.string.app_name)) },
+                modifier = Modifier.statusBarsPadding(),
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceContainer
+                ),
+                navigationIcon = {
+                    IconButton(onClick = {
+                        coroutineScope.launch {
+                            drawerState.open()
+                        }
+                    }) {
+                        Icon(Icons.Default.Menu, contentDescription = "Menu")
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            modifier = Modifier.padding(innerPadding),
+            startDestination = Destination.Home
+        ) {
+            composable<Destination.Home> {
+                ContentScreen(Destination.Home.label)
+            }
+            composable<Destination.Shopping> {
+                ContentScreen(Destination.Shopping.label)
+            }
+            composable<Destination.Favorites> {
+                ContentScreen(Destination.Favorites.label)
+            }
+            composable<Destination.Calendar> {
+                ContentScreen(Destination.Calendar.label)
+            }
+            composable<Destination.Bin> {
+                ContentScreen(Destination.Bin.label)
+            }
         }
     }
 }
